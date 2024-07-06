f10988f45f9628cd83ee6bee14c388d78583620e
aanxniee <caishajia@gmail.com>
2024-07-05 18:39:26 -0700

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,115 @@
-import Image from "next/image";
+import { Box, Flex, Text, Link, Image, VStack, HStack, Container, Highlight, ListItem, List } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Container maxW="container.xl" height="100vh">
+      <Flex
+        direction={{ base: 'column', md: 'row' }}
+        height="100%"
+        align="center"
+        justify="center"
+        className="home"
+      >
+        <Box flex="1" p="5">
+          <VStack spacing={4} align="center">
+            <HStack spacing={8} justify="flex-end" width="100%">
+              <Link as={NextLink} href="/resume" passHref><Text as="a">resume</Text></Link>
+              <Link as={NextLink} href="/projects" passHref><Text as="a">projects</Text></Link>
+            </HStack>
+            <HStack spacing={4}>
+              <VStack spacing={4}>
+                <Image borderRadius="full" src="/images/bay_bridge.jpg" alt="Bay Bridge" boxSize="70px" objectFit="cover" />
+              </VStack>
+              <VStack spacing={4}>
+                <Box height="30px" />
+                <Image borderRadius="full" src="/images/coit.jpg" alt="San Francisco" boxSize="120px" objectFit="cover" />
+              </VStack>
+              <VStack spacing={4}>
+                <Image borderRadius="full" src="/images/annie.jpg" alt="Annie" boxSize="200px" objectFit="cover" />
+              </VStack>
+              <VStack spacing={4}>
+                <Box height="90px" />
+                <Image borderRadius="full" src="/images/toronto.jpg" alt="Toronto" boxSize="120px" objectFit="cover" />
+              </VStack>
+              <VStack spacing={4}>
+                <Box height="40px" />
+                <Image borderRadius="full" src="/images/ocean_beach.jpg" alt="Ocean Beach" boxSize="80px" objectFit="cover" />
+              </VStack>
+            </HStack>
+            <Text fontSize="2xl" fontWeight="bold">
+              Hey, I'm Annie
+            </Text>
+            <Text>
+              I'm studying
+              <Highlight
+                query={["Systems Design Engineering", "University of Waterloo"]}
+                styles={{ px: '2', py: '0', bg: '#A2C6EA', rounded: 'full', lineHeight: '0.5' }}
+              >
+                Systems Design Engineering at the University of Waterloo
+              </Highlight>
+            </Text>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <VStack pt={5} spacing={0}>
+              <Text px={2} lineHeight="1.3">
+                <Text as="span" fontWeight="bold" color="#76A2CE">excited for</Text> longer days, new homes in tech
+              </Text>
+              <Text px={2} lineHeight="1.3">
+                <Text as="span" fontWeight="bold" color="#76A2CE">inspired by</Text> big cities, large windows and tall buildings, people that love what they do
+              </Text>
+            </VStack>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <VStack width="600px" pt={5} spacing={4} align="start">
+              <Text>recently, I...</Text>
+              <List spacing={3} pl={14} styleType="disc">
+                <ListItem lineHeight="1.2">
+                  am working at
+                  <Link href="https://www.newfront.com" isExternal textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                    <Text as="span" mx={1} fontWeight="bold" borderBottom="3px solid #76A2CE" color="#76A2CE">
+                      Newfront
+                    </Text>
+                  </Link> in San Francisco this summer - excited to dive deep into fintech and explore the west coast!
+                </ListItem>
+                <ListItem lineHeight="1.2">
+                  am leading a new team of 9 developers and designers to build tooling for the Humane Society with
+                  <Link href="https://www.uwblueprint.org" isExternal textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                    <Text as="span" mx={1} fontWeight="bold" borderBottom="3px solid #76A2CE" color="#76A2CE">
+                      UW Blueprint
+                    </Text>
+                  </Link>
+                </ListItem>
+                <ListItem lineHeight="1.2">
+                  spent autumn in Downtown Toronto at
+                  <Link href="https://www.rbc.com/cyber-security/" isExternal textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                    <Text as="span" mx={1} fontWeight="bold" borderBottom="3px solid #76A2CE" color="#76A2CE">
+                      Royal Bank of Canada
+                    </Text>
+                  </Link>
+                  discovering some pretty interesting work in this problem space
+                </ListItem>
+              </List>
+              <Text>and in other moments I'm...</Text>
+              <List spacing={3} pl={14} styleType="disc">
+                <ListItem lineHeight="0.7">exploring nature trails and parks, enjoying the warmer weather that California has to offer</ListItem>
+                <ListItem lineHeight="0.7">watching sunrises and sunsets in silence, taking in how pretty the world really is</ListItem>
+                <ListItem lineHeight="0.7">trying to find the best matcha and pastries in the city (any recs for SF?) </ListItem>
+              </List>
+            </VStack>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <HStack spacing={4} alignSelf="flex-start" pt={5}>
+              <Link href="https://github.com/aanxniee" isExternal>
+                <Image src="/images/github.png" alt="GitHub" boxSize="24px" />
+              </Link>
+              <Link href="https://www.linkedin.com/in/anniecai2004" isExternal>
+                <Image src="/images/linkedin.png" alt="LinkedIn" boxSize="24px" />
+              </Link>
+              <Link href="mailto:ajcai@uwaterloo.ca" isExternal>
+                <Image src="/images/mail.png" alt="Mail" boxSize="24px" />
+              </Link>
+            </HStack>
+          </VStack>
+        </Box>
+      </Flex>
+    </Container >
   );
 }
