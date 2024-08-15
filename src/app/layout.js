a1b036a5cd65473478b454ac39de264faeec7e1a
aanxniee <caishajia@gmail.com>
2024-08-15 16:48:17 -0700

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Providers } from "./providers";
 import { EB_Garamond } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 export const eb_garamond = EB_Garamond({
   subsets: ["latin"],
@@ -16,7 +17,12 @@ export default function RootLayout({ children }) {
   const classNames = `${eb_garamond.className} bg-neutral-100`;
 
   return (
-    <html>
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={classNames}>
         <Providers>{children}</Providers>
       </body>
